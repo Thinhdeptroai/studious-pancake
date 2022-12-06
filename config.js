6e8c6447959d2e7fe2575c802a6649f87aa860d2
Thinhdeptroai <119950013+Thinhdeptroai@users.noreply.github.com>
2022-12-06 21:21:31 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Gửi cho người nào đó",
+    introTitle: 'Hình như tớ thích 1 người rồi áaaa!',
+    introDesc: `Cô ấy khom như gu tớ, hay khóc nè, hay overthinking, lại còn là 1 nữ cường nựa... nhma cô ấy gất là dễ thương nhaaaaaa với quan tâm tớ nựa, xỉu mất hoi écccc`,
+    btnIntro: '^^HiHi^^',
+    title: 'Cậu biết hem, tớ thích cô ấy vì cô ấy y như tớ hồi đó, đều mất niềm tin vào tình yêu',
+    desc: 'Nhma nếu cậu là cô ấy, thì cậu có thích 1 con người hay quan tâm thái quá và simp quá mức không, có thể sẽ phiền đó huhu ',
+    btnYes: 'Vẫn cứ là thích nhaaaaaa <33',
+    btnNo: 'Không, cậu quá trẻ con',
+    question: 'Cô ấy có rất rất nhiều điểm tốt mà, như bầu trời đầy sao lun á, nhưng mà tớ có nên tỏ tình khom nhỉ, huhu?',
+    btnReply: 'Ơ, tại sao đấyyyyyyyy, tớ có gì tốt đâu mà',
+    reply: 'Cóoooooooooo',
+    mess: 'Tớ biết mà. Tớ sẽ cố gắng để tỏ tình và mang lại cho cô ấy hạnh phúc nhiều ơi là nhiều',
+    messDesc: 'Để tớ trao cô ấy những điều trân thành và dịu dàng nhất.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
